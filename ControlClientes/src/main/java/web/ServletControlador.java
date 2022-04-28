22d8dc9444cc87d5530e9a82d403baeca54208a1
Napoleón Sáenz <napoleonsaenz85@gmail.com>
2022-04-27 23:05:21 -0600

    JSP - SERVLET
    Creacion de index.jsp
    Creacion de clientes.jsp
    Creacion y Configuración de ServeltController

---
+++ ControlClientes/src/main/java/web/ServletControlador.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package web;
+
+import datos.ClienteDaoJDBC;
+import dominio.Cliente;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author HUESPED
+ */
+@WebServlet(name = "ServletControlador", urlPatterns = {"/ServletControlador"})
+public class ServletControlador extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        /* ...*/
+        List<Cliente> clientes = new ClienteDaoJDBC().listar();
+        
+        System.out.println("clientes = "+clientes);
+        request.setAttribute("clientes", clientes);
+        request.getRequestDispatcher("clientes.jsp").forward(request, response);
+    }
+
+
+
+}
